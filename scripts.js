7fb435eb34b798e443a8ad619cab8a2976dfd3c9
Steve Smith <stevesmithsays@Steves-MacBook-Pro.local>
2018-05-09 10:10:16 -0500

    binary function

--- scripts.js
+++ scripts.js
@@ -6,39 +6,49 @@
 // -If he doesn't get 10 hoops, return the string "Keep at it until you get it".
 
 // CODE HERE
-
-function hoopCount(n){
+const hoopCount = (n) =>{
   if(n >= 10){
     return "Great, now move on to tricks"
-    }
+  }
     else{
-    return "Keep at it until you get it"
+      return "Keep at it until you get it"
     }
 }
 
 // Your task is to create a function that does four basic mathematical operations.
-
 // The function should take three arguments - operation(string/char), value1(number), value2(number).
 // The function should return result of numbers after applying the chosen operation.
 
 // CODE HERE
-
-// const basicOp = (op, a, b) => {
-//   if(op === "+"){
-//   return a + b
-//   }
-//   else if(op === "-"){
-//   return a -= b
-//   }
-//   else if(op === "*"){
-//     return a *= b
-//   }
-//   else if(op === "/"){
-//   return a /= b
-//   }
-// }
-
+const basicOp = (op, a, b) => {
+  if(op === "+"){
+  return a + b
+  }
+  else if(op === "-"){
+  return a -= b
+  }
+  else if(op === "*"){
+    return a *= b
+  }
+  else if(op === "/"){
+  return a /= b
+  }
+}
 // basicOp('+', 4, 7)         // Output: 11
 // basicOp('-', 15, 18)       // Output: -3
 // basicOp('*', 5, 5)         // Output: 25
 // basicOp('/', 49, 7)        // Output: 7
+
+
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+// The binary number returned should be a string.
+// CODE HERE
+
+// ** ORIGINAL SOLUTION ** //
+// function addBinary(a,b) {
+//   let sum = a + b;
+//   return (sum >>>0).toString(2);
+//  }
+
+const addBinary = (a, b) => {let c = a+b; return (c >>> 0).toString(2)}
+
